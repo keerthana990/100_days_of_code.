2937dd64b69b6671d68727126d5adf9f84306c7d
keerthana990 <keerthana20030311@gmail.com>
2025-01-05 21:03:01 +0530

    update

--- Day5.py
+++ Day5.py
@@ -7,26 +7,26 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-# #type 1
-# letters_created=random.sample(letters,nr_letters)
-# symbols_created=random.sample(symbols,nr_symbols)
-# numbers_created=random.sample(numbers,nr_numbers)
-# password=symbols_created+numbers_created+letters_created
-# random.shuffle(password)
-# password_created=''.join(password)
-# print("password:"+password_created)
+#method 1
+letters_created=random.sample(letters,nr_letters)
+symbols_created=random.sample(symbols,nr_symbols)
+numbers_created=random.sample(numbers,nr_numbers)
+password=symbols_created+numbers_created+letters_created
+random.shuffle(password)
+password_created=''.join(password)
+print("password:"+password_created)
 
-#type2
-# password=""
-# for l in range(1,nr_letters+1):
-#     password+=random.choice(letters)
-# for n in range(1,nr_numbers+1):
-#     password+=random.choice(numbers)
-# for s in range(1,nr_symbols+1):
-#     password+=random.choice(symbols)
-# print("password: "+password)
+# method 2
+password=""
+for l in range(1,nr_letters+1):
+    password+=random.choice(letters)
+for n in range(1,nr_numbers+1):
+    password+=random.choice(numbers)
+for s in range(1,nr_symbols+1):
+    password+=random.choice(symbols)
+print("password: "+password)
 
-#type3
+#method 3
 password_l=[]
 for l in range(1,nr_letters+1):
     password_l+=random.choice(letters)
